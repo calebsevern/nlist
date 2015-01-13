95d208b56249204bcb8400bdd7fa64481069651d
calebsevern <calebsevern@gmail.com>
2015-01-13 17:00:57 -0500

    Localized jQuery

---
+++ pages/navigation.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html>
+	<head>
+		<link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+		<link rel="stylesheet" href="css/styles.css">
+		<title>nlist | Home</title>
+	</head>
+	<body>
+		<div class="nav">
+			<div class="logo">
+				nlist
+				<i class="fa fa-home"></i>
+			</div>
+			<div class="tab"></div>
+			<div class="label">Experiments</div>
+			<div class="tab" data-url="admin/newexperiment.php">Create New</div>
+			<div class="tab" data-url="admin/currentexperiments.php">Current</div>
+			<div class="tab" data-url="admin/completedexperiments.php">Completed</div>
+			<div class="tab" data-url="admin/currentexperiments.php">Overview</div>
+			
+			<div class="tab"></div>
+			<div class="label">Participants</div>
+			<div class="tab">Create New</div>
+			<div class="tab">Overview</div>
+			
+			<div class="tab"></div>
+			<div class="label">Utilities</div>
+			<div class="tab">Documents</div>
+			<div class="tab">Settings</div>
+			<div class="tab">Statistics</div>
+			<div class="tab"></div>
+			<div class="tab logout">Logout</div>
+		</div>
+		<iframe class="content-pane" src=""></iframe>
+		
+		<script src="js/jquery.2.1.1.min.js"></script>
+		<script src="js/script.js"></script>
+		<script src="js/db.js"></script>
+		<script src="js/db-test.js"></script>
+	</body>
+</html>
